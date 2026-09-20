fba1388b3c7bb807080c12faa64565fefc4e6fb0
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2026-09-20 08:25:04 +0200

    🐛 fix xlsx package imports

--- src/lib/utils/xlsxParser.spec.ts
+++ src/lib/utils/xlsxParser.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { utils, type WorkBook } from 'xlsx';
+import { utils, type WorkBook } from '@e965/xlsx';
 
 import { processWorkbook } from './xlsxParser';
 
